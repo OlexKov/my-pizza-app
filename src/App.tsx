d94eb96837536a87ef6e993d5aaea05604cbd1be
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-12 20:57:00 +0300

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import CategoriesPage from "./components/categories/CategoriesPage";
+
+
+function App() {
+  return (
+   <CategoriesPage/>
+  );
+}
+
+export default App;
---
+++ src/components/categories/CategoriesPage.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { ICategoryItem } from './types'
+
+const CategoriesPage: React.FC = () => {
+
+  const[list,setList] = useState<ICategoryItem[]>([
+
+  ])
+  return (
+    <>
+    </>
+  )
+}
+
+export default CategoriesPage

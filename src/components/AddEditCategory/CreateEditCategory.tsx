12837b2fb1563149caaf845dcb60493100ac3724
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-17 14:23:57 +0300

    Update

--- src/components/AddEditCategory/CreateEditCategory.tsx
+++ src/components/AddEditCategory/CreateEditCategory.tsx
@@ -1,40 +1,44 @@
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import PageHeader from '../PageHeader/PageHeader'
 import { categoryService } from '../../services/CategoryService';
 import { useNavigate } from 'react-router-dom';
+import { FieldValues, useForm } from 'react-hook-form';
+
 
 
 
 const CreateEditCategory: React.FC = () => {
   const navigate = useNavigate()
   const [preview, setPreview] = useState<string>('')
-  const [file, setFile] = useState<File>()
-  
+  //const [file, setFile] = useState<File>()
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
-      setFile(file)
+      //setFile(file)
       setPreview(URL.createObjectURL(file));
     }
   }
 
-  const onSubmit = async () => {
-     
-    // const formData = new FormData();
-    // formData.append('name', data.name);
-    // if (file)
-    //   formData.append('image', file);
-    // const result = await categoryService.create(formData);
-    // if (result.status === 201) {
-    //   navigate(-1);
-    // }
+
+
+  const onSubmit = async (data: FieldValues) => {
+
+    const formData = new FormData();
+    formData.append('name', data.name);
+    if (data.image.length > 0)
+      formData.append('image', data.image[0]);
+    const result = await categoryService.create(formData);
+    if (result.status === 201) {
+      navigate(-1);
+    }
   }
 
   return (
     <>
       <PageHeader title="Нова категорія" />
-      <form onSubmit={onSubmit} className="container max-w-sm w-auto mx-auto items-center py-32" encType="multipart/form-data" >
+      <form onSubmit={handleSubmit(onSubmit)} className="container max-w-sm w-auto mx-auto items-center py-32" encType="multipart/form-data" noValidate >
         <div className="sm:col-span-2 mb-5">
           <label htmlFor="name" className="block text-lg mb-5 font-medium leading-4 text-gray-400">
             Назва категорії
@@ -45,9 +49,17 @@ const CreateEditCategory: React.FC = () => {
               id="name"
               className="peer border-2 focus:ring-2 [.validated_&]:invalid:border-pink-600 [.validated_&]:invalid:ring-2 [.validated_&]:invalid:ring-pink-200  block w-full rounded-md px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               required
-            
+              {...register('name', {
+                required: 'Ведіть назву категорії',
+                minLength: {
+                  value: 3,
+                  message: 'Назва категорії не може бути менша як 3 символи',
+                },
+              })}
             />
-           
+            {errors.name && (
+              <p className="text-xl italic text-red-500">{errors.name?.message?.toString()}</p>
+            )}
           </div>
         </div>
         <div id="img-uploader" className=" mx-auto">
@@ -62,17 +74,32 @@ const CreateEditCategory: React.FC = () => {
                     <img src={preview} className="max-h-48 rounded-lg mx-auto" alt="Image preview" />
                   </>
                   : <>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 text-gray-700 mx-auto mb-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 text-gray-700 mx-auto mb-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                     </svg>
                     <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-700">Завантажте зображення</h5>
                     <p className="font-normal text-sm text-gray-400 md:px-6">Оберіть фото для категорії</p>
                     <p className="font-normal text-sm text-gray-400 md:px-6"><b className="text-gray-600">JPG, PNG, or GIF</b> format.</p>
                   </>}
               </label>
-              <input id="image" name="image" onChange={onImageChange} type="file" className="hidden" accept="image/*" required />
+              <input
+                id="image"
+                type="file"
+                className="hidden"
+                accept="image/*"
+                required
+                {...register('image', {
+                  onChange:(e)=>{onImageChange(e)},
+                  required: 'Оберіть фото категорії',
+                })}
+               
+              />
+             
             </div>
           </div>
+          {errors.image && (
+                <p className="text-xl italic text-red-500">{errors.image?.message?.toString()}</p>
+              )}
         </div>
         <button type="submit"
           className="rounded-md bg-indigo-600 mt-5 w-full px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Додати</button>

4b4e6104cc608063adcf50e7aa57d3af5db64144
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-19 13:23:13 +0300

    Update

--- src/components/AddEditCategory/CreateEditCategory.tsx
+++ src/components/AddEditCategory/CreateEditCategory.tsx
@@ -52,7 +52,7 @@ const CreateEditCategory: React.FC = () => {
 
   return (
     <>
-      <PageHeader title="Нова категорія" />
+      <PageHeader title={id===0?'Нова категорія':'Редагування'} />
       <form onSubmit={handleSubmit(onSubmit)} className="container max-w-sm w-auto mx-auto items-center py-32" encType="multipart/form-data" noValidate >
         <div className="sm:col-span-2 mb-5">
           <label htmlFor="name" className="block text-lg mb-5 font-medium leading-4 text-gray-400">
@@ -116,7 +116,7 @@ const CreateEditCategory: React.FC = () => {
               )}
         </div>
         <button type="submit"
-          className="rounded-md bg-indigo-600 mt-5 w-full px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Додати</button>
+          className="rounded-md bg-indigo-600 mt-5 w-full px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Зберегти</button>
       </form>
     </>
   )

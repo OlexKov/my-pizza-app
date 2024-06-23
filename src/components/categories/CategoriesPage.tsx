60709f2b0a0fad26a5ca4d7fa9ce2e01d6e4c1f8
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-23 11:15:50 +0300

    Update

--- src/components/categories/CategoriesPage.tsx
+++ src/components/categories/CategoriesPage.tsx
@@ -16,8 +16,8 @@ const CategoriesPage: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [list, setList] = useState<ICategoryItem[]>([]);
   const [total, setTotal] = useState<number>(0);
-  const [currentPage,setCurrentPage] = useState<number>(1)
-  const [perPage,setPerPage] = useState<number>(2)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [perPage, setPerPage] = useState<number>(2)
   const [loading, setLoading] = useState<boolean>(true);
   const [progress, setProgress] = useState<number>(0)
 
@@ -33,9 +33,9 @@ const CategoriesPage: React.FC = () => {
 
   useEffect(() => {
     (async () => {
-
+      setLoading(true)
       const search: string | null = searchParams.get("search")
-      const response = await categoryService.getList(search || '', currentPage , perPage  , prog);
+      const response = await categoryService.getList(search || '', currentPage, perPage, prog);
       if (response.status === 200) {
         console.log('data->', response.data)
         setTotal(response.data.total)
@@ -43,50 +43,56 @@ const CategoriesPage: React.FC = () => {
         setPerPage(response.data.per_page)
         setList(response.data.data)
         setLoading(false)
+        setProgress(0);
       }
     })()
-  }, [searchParams,currentPage,perPage])
+   
+  }, [searchParams, currentPage, perPage])
 
-  const onSearch :SearchProps['onSearch'] = (value) => {
-      setSearchParams({ search: `${value}` })
+  const onSearch: SearchProps['onSearch'] = (value) => {
+    setSearchParams({ search: `${value}` })
   }
 
-  const onPaginationChange:PaginationProps['onShowSizeChange'] = (current, pageSize) => {
-     console.log(current,pageSize)
-     setCurrentPage(current);
-     setPerPage(pageSize);
-   }
+  const onPaginationChange: PaginationProps['onShowSizeChange'] = (current, pageSize) => {
+    console.log(current, pageSize)
+    setCurrentPage(current);
+    setPerPage(pageSize);
+  }
 
   return (
     <>
-      
+
       <div className={"md:container mx-auto flex flex-col"}>
         <h1 className={"text-center my-[20px] text-3xl sm:text-3xl text-slate-900 tracking-tight dark:text-slate-700"}>Меню</h1>
-        <Search  className=' mb-14' size="large"  placeholder="Пошук по назві" allowClear onSearch={onSearch} />
-        <div className={"grid  place-items-center grid-cols-1  sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4"}>
-          {list.map(item => (
-            <div key={item.id} className="max-w-sm rounded overflow-hidden shadow-lg">
-              <img className="w-full" src={imageUrl + '600_' + item.image} alt="Sunset in the mountains" />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2 text-center">{item.name}</div>
-              </div>
+        <Search className=' mb-14' size="large" placeholder="Пошук по назві" allowClear onSearch={onSearch} />
+        {!loading && list.length > 0 &&
+          <>
+            <div className={"grid  place-items-center grid-cols-1  sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4"}>
+              {list.map(item => (
+                <div key={item.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                  <img className="w-full" src={imageUrl + '600_' + item.image} alt="Sunset in the mountains" />
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2 text-center">{item.name}</div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        {list.length > 0 &&
-        <Pagination
-          current={currentPage}
-          showSizeChanger
-          onChange={onPaginationChange}
-          defaultCurrent={1}
-          total={total}
-          pageSizeOptions={[2,4,8,10]}
-          pageSize ={perPage}
-          className=' mt-10  mr-14 self-end'
-        />}
+            <Pagination
+              current={currentPage}
+              showSizeChanger
+              onChange={onPaginationChange}
+              defaultCurrent={1}
+              total={total}
+              pageSizeOptions={[2, 4, 8, 10]}
+              pageSize={perPage}
+              className=' mt-10  mr-14 self-end'
+            />
+          </>
+        }
       </div>
-      {!loading && list.length === 0 && <Empty className=' mx-auto mt-6' />}
       {loading && <Progress percent={progress} success={{ percent: progress }} />}
+      {!loading && list.length === 0 && <Empty className=' mx-auto mt-6' />}
+
     </>
   )
 }
